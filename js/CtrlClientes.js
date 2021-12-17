518ed671b8b72a6f701f2352c20c726c4e07a1f0
thecrystalbakery3 <95950235+thecrystalbakery3@users.noreply.github.com>
2021-12-17 16:10:44 -0600

    Update CtrlClientes.js

--- js/CtrlClientes.js
+++ js/CtrlClientes.js
@@ -79,7 +79,7 @@ function htmlFila(doc) {
   return ( /* html */
     `<li>
       <a class="fila" href=
-  "alumno.html?${parámetros}">
+  "cliente.html?${parámetros}">
         <strong class="primario">
           ${nombre} ${direccion} ${dformat}
         </strong>
